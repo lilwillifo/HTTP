2997f790bfb92d3bce068297fbad4ff3ceaeb063
lilwillifo <margaretwilliford4@gmail.com>
2018-02-12 08:14:50 -0700

    iteration 0 working

--- lib/server.rb
+++ lib/server.rb
@@ -5,22 +5,26 @@ class Server
   end
 
   def start
-    client = @tcp_server.accept
+    counter = 0
+    loop do
+      client = @tcp_server.accept
 
-    request_lines = []
-    while line = client.gets and !line.chomp.empty?
-      request_lines << line.chomp
+      request_lines = []
+      while line = client.gets and !line.chomp.empty?
+        request_lines << line.chomp
+      end
+
+      response = "<pre>" + "Hello World!(#{counter})" + "</pre>"
+      output = "<html><head></head><body>#{response}</body></html>"
+      headers = ["http/1.1 200 ok",
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+      client.puts headers
+      client.puts output
+      counter += 1
     end
-
-    response = "<pre>" + "Hello World (0)" + "</pre>"
-    output = "<html><head></head><body>#{response}</body></html>"
-    headers = ["http/1.1 200 ok",
-              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-              "server: ruby",
-              "content-type: text/html; charset=iso-8859-1",
-              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    client.puts headers
-    client.puts output
     client.close
   end
 
--- test/server_test.rb
+++ test/server_test.rb
@@ -5,7 +5,19 @@ require_relative 'test_helper'
 class ServerTest < Minitest::Test
   def test_hello_returns_expected_body
     response = Faraday.get 'http://127.0.0.1:9292/'
+    expect = '<html><head></head><body><pre>Hello World!(0)</pre></body></html>'
 
-    assert_equal 'Hello, World! (0)', response.body
+    assert_equal expect, response.body
+
+    response = Faraday.get 'http://127.0.0.1:9292/'
+    expect = '<html><head></head><body><pre>Hello World!(1)</pre></body></html>'
+
+    assert_equal expect, response.body
+
+    response = Faraday.get 'http://127.0.0.1:9292/'
+    expect = '<html><head></head><body><pre>Hello World!(2)</pre></body></html>'
+
+    assert_equal expect, response.body
   end
+
 end
