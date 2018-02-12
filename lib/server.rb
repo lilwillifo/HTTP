b85ae0e77ac3766606f2689028e1687eaf061512
lilwillifo <margaretwilliford4@gmail.com>
2018-02-12 07:57:10 -0700

    connection to port working

--- lib/server.rb
+++ lib/server.rb
@@ -1,22 +1,27 @@
-
 require 'socket'
-
 class Server
-  attr_reader :tcp_server, :client
   def initialize
-    @server = TCPServer.new(9292)
-    @request_count = 0
+  @tcp_server = TCPServer.new(9292)
   end
 
   def start
-    client = @server.accept
-    get_request(client)
-  end
+    client = @tcp_server.accept
 
-  def get_request(client)
-    request = Request.new(client)
-    request.save_request
-    @request_count += 1
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+
+    response = "<pre>" + "Hello World (0)" + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = ["http/1.1 200 ok",
+              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    client.puts headers
+    client.puts output
     client.close
   end
+
 end
--- test/server_test.rb
+++ test/server_test.rb
@@ -3,15 +3,9 @@ require './lib/server'
 require_relative 'test_helper'
 
 class ServerTest < Minitest::Test
-  def test_it_exists
-    server = Server.new
-    assert_instance_of Server, server
-  end
-
   def test_hello_returns_expected_body
     response = Faraday.get 'http://127.0.0.1:9292/'
 
     assert_equal 'Hello, World! (0)', response.body
   end
-
 end
